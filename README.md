f90c406c452b62d578b543f939d7f4a94d982530
Josh Whetton <>
2022-08-07 10:50:58 -0400

    Working POC

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # abbreav
-abbrv
+
+Abbrevation bot for discord.
+
+### Configuration
+
+Copy `example-config.toml` to `config.toml`. Update the bot token and any initial abbreviations you would like to register. Bot will delete the command requesting the expansion and post a new message with the expanded form.
+
+```toml
+[secrets]
+# Secrets required for the bot to run.
+token = TOKEN_HERE
+
+[abbr]
+# Abbreviations registered with the bot.
+demo = "this is a demo"
+nc = "/newCommand arg1 arg2 argN"
+```
+
+### Usage
+
+Built-in commands:
+
+- `save`: Write the current config to `config.toml`. This _saves_ any changes you've made.
+
+Adding abbreviations is done like so: `>ab ABBR VALUE [...]`. Example `>ab nc /newCommand 123 234` where `nc` will then expand to `/newCommand 123 234`.
+
+Using abbreviations:
+
+Have the bot expand your abbreviation by using `>ab abbreviation` such as `>ab nc` to expand the example we set earlier.
--- abbrv.py
+++ abbrv.py
@@ -3,17 +3,32 @@
 import discord
 from discord.ext import commands
 import os
+import tomli
+import tomli_w
+
+config_filename = "config.toml"
+try:
+    with open(config_filename, "rb") as config_file:
+        config = tomli.load(config_file)
+        print("found and loaded config from disk!")
+except OSError:
+    print("could not find config on disk. using defaults and env for secrets.")
+    config = {}
+    config["secrets"] = {"token": os.getenv("token")}
+    config["abbr"] = {}
+    config["abbr"] = {"ping": "pong"}
+
+
+print(f">>> loaded {len(config['abbr'])} abbreviations.")
 
 intents = discord.Intents.default()
 # intents.message_content = True
-bot = commands.Bot(command_prefix='>', intents=intents)
-abv_trigger = ">ab"
-abbreviations = {}
-abbreviations["/demo"] = "this is a demo"
+bot = commands.Bot(command_prefix=">", intents=intents)
+
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send("pong")
 
 
 @bot.command()
@@ -21,16 +36,26 @@ async def ab(ctx):
     """Set and fix abbreviations"""
     # maybe this should be ctx.args instead.
     match ctx.message.content.split():
-        case [abv_trigger, abbrev]:
+        case [">ab", "save"]:
+            try:
+                with open(config_filename, "wb") as config_file:
+                    tomli_w.dump(config, config_file)
+                await ctx.message.channel.send(content="wrote config!")
+                print(f"wrote current config to {config_filename}")
+            except OSError as e:
+                await ctx.message.channel.send(
+                    content=f"failed to write config file: {e}"
+                )
+        case [">ab", abbrev]:
             print(f"got request to replace {abbrev}")
-            # need to see how avrae does this. i get a perm error.
-            #await ctx.message.edit(content=abbreviations[abbrev])
-            await ctx.message.reply(content=f"i would replace that with: {abbreviations[abbrev]}")
-        case [abv_trigger, abbrev, *rest]:
+            await ctx.message.delete()
+            await ctx.message.channel.send(content=config["abbr"][abbrev])
+        case [">ab", abbrev, *rest]:
             value = " ".join(rest)
-            print(f"going to set a new abbreviation => {abbrev}: '{value}'")
-            abbreviations[abbrev] = value
-            await ctx.message.reply(content=f"i set {abbrev} to: '{value}'")
+            config["abbr"][abbrev] = value
+            print(f"set a new abbreviation => {abbrev}: '{value}'")
+            await ctx.message.delete()
+            await ctx.message.channel.send(content=f"set '{abbrev}': '{value}'")
 
 
-bot.run(os.getenv("abbrv_token"))
+bot.run(config["secrets"]["token"])
